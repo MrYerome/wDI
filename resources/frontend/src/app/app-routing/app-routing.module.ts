2b6aa6a47c6e1091817024451ab0b0450f781dd7
Yerome <vinet.jerome@gmail.com>
2018-11-22 15:34:37 +0100

    ajout node_modules + accueil

--- resources/frontend/src/app/app-routing/app-routing.module.ts
+++ resources/frontend/src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {AccueilComponent} from '../accueil/accueil.component';
 import {AppComponent} from "../app.component";
 import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+import {TestComponent} from "../test/test.component";
 const ROUTES: Routes = [
   {path:'', component : AppComponent  },
   // {path : '**' , component : PageNotFoundComponent},
-  {path:'accueil', component : AccueilComponent  }
+  {path:'accueil', component : AccueilComponent  },
+  {path:'test', component : TestComponent  }
 ]
 
 @NgModule({
--- resources/frontend/src/app/app.module.ts
+++ resources/frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {AccueilComponent} from "./accueil/accueil.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AppRoutingModule} from "./app-routing/app-routing.module";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
